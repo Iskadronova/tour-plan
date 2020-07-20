94772192d29e126a2c36e71ece8209a8c2a3de8c
Iskadronova <Candy__88@mail.ru>
2020-07-21 03:42:49 +0500

    Create: Added arrow in slider

--- js/main.js
+++ js/main.js
@@ -2,14 +2,9 @@ var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
   },
 });
