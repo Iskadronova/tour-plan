3186b2e05d8d6bcde7a9f2fe9f4ef0c2508fecd8
Iskadronova <Candy__88@mail.ru>
2020-07-21 04:06:19 +0500

    Create: added switching from the keyboard

--- js/main.js
+++ js/main.js
@@ -7,4 +7,9 @@ var mySwiper = new Swiper(".swiper-container", {
     nextEl: ".slider-button--next",
     prevEl: ".slider-button--prev",
   },
+
+  // Управление с клавиатуры
+  keyboard: {
+    enabled: true,
+  },
 });
