2c95873545d7100fc9341c7b37c2d10eb4fd92e2
Iskadronova <Candy__88@mail.ru>
2020-07-31 02:11:42 +0500

    create: newsletter subscription for send.php

--- send.php
+++ send.php
@@ -54,3 +54,50 @@ else {$result = "error";}
 
 // Отображение результата
 header('Location: thankyou.html');
+
+
+// Переменные, которые отправляет пользователь
+$email = $_POST['email'];
+
+// Формирование самого письма
+$title = "Рассылка Best Tour Plan";
+$body = "
+<h2>Новостная рассылка Best Tour Plan</h2>
+<b>Сообщение:</b><br> Мы рады видеть Вас в числе наших подписчиков!";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'iskadronovas@gmail.com'; // Логин на почте
+    $mail->Password   = '!QAZxsw2'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('iskadronovas@gmail.com', 'Светлана Искадронова'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress($email);
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: newsletter.html');
