fecd31f336853b9614a1593cc7c6b261231f0269
Iskadronova <Candy__88@mail.ru>
2020-08-01 22:00:43 +0500

    create: added modal windows & adaptive

--- js/main.js
+++ js/main.js
@@ -1,39 +1,65 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button_next",
-    prevEl: ".hotel-slider__button_prev",
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button_next",
+      prevEl: ".hotel-slider__button_prev",
+    },
 
-  // Управление с клавиатуры
-  keyboard: {
-    enabled: true,
-  },
-});
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
+    // Управление с клавиатуры
+    keyboard: {
+      enabled: true,
+    },
+  });
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button_next",
-    prevEl: ".reviews-slider__button_prev",
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button_next",
+      prevEl: ".reviews-slider__button_prev",
+    },
 
-  // Управление с клавиатуры
-  keyboard: {
-    enabled: true,
-  },
-});
-$(".parallax-window").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
+    // Управление с клавиатуры
+    keyboard: {
+      enabled: true,
+    },
+  });
+  $(".parallax-window").parallax({ imageSrc: "./img/newsletter-bg.jpg" });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom_visible");
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    //console.log("Клик по кнопке меню");
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  // Вывод данных переменной в консоль
+  //console.log(modalButton);
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  $(document).on("keydown", function (event) {
+    if (event.keyCode == 27) closeModal(event);
+  });
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    console.log(targetModal);
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
