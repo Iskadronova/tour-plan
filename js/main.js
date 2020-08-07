87038e6f1e4af0d5798c64ebc926dbeda45add8b
Iskadronova <Candy__88@mail.ru>
2020-08-08 03:16:00 +0500

    create: Loading the map on mouse hover

--- js/main.js
+++ js/main.js
@@ -97,6 +97,14 @@ $(document).ready(function () {
   //Initialize AOS
   AOS.init();
 
+  //Загрузка карты по наведению
+  $(".google-map").hover(function () {
+    $(this).attr(
+      "src",
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.004681430924!2d79.80141631528154!3d7.5744669945377785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2c96110de8289%3A0x3fd7668bc2d85eb9!2sGrand%20Hilton%20Hotel!5e0!3m2!1sru!2sru!4v1595368124164!5m2!1sru!2sru"'
+    );
+  });
+
   //Загрузка изображений после загрузки страницы
   function init() {
     var imgDefer = document.getElementsByTagName("img");
